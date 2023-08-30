3be75f2a61bd7333e3ce5635438994937d6316d8
Harshit Kumar Sahu <https://github.com/Tysondgn>
2023-08-31 01:06:28 +0530

    A

--- sidekick.py
+++ sidekick.py
@@ -1,25 +1,93 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, session, redirect
 from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
 import json
 import os
 
 app = Flask(__name__)
 
-
+app.secret_key = 'your secret key'
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'sidekick'
+ 
+mysql = MySQL(app)
+ 
 # A decorator used to tell the application
 # which URL is associated function
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def home():
-    if request.method == "POST":
-        # getting input with name = fname in HTML form
-        phone_number = request.form.get("phone")
-        return phone_number
     return render_template('Home.html')
 
 
 @app.route("/project")
 def project():
     return render_template('project.html')
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')
+
+
+@app.route('/login', methods =['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('home.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+ 
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    # return redirect(url_for('login'))
+    return redirect(url_for('home'))
+ 
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'fname' in request.form and 'lname' in request.form and 'phone' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        first_name = request.form['fname']
+        last_name = request.form['lname']
+        phone = request.form['phone']
+        print(phone)
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers !'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form !'
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s, % s, % s, % s)', (username, password, email, first_name, last_name, phone,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
 
 
 app.run(debug=True)
